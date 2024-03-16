ec111f8743d9f25928aa93aca4df89394ddc3d95
Chorym <pham25052005@gmail.com>
2024-03-16 09:02:00 +0700

    Add project files.

---
+++ Pikichu game/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+/*
+Notes:
+
+function name should be writen like this:
+type functionName(){}
+camel case
+
+Variable should be:
+type variable_name;
+snake case
+
+Struct/Class should be:
+Struct/Class Item(){};
+one word, captital first letter
+
+
+*/
+
+int** generateGameBoard()
+{
+
+}
+
+int main()
+{
+	return 0;
+}
